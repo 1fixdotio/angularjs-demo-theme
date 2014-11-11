e3d0d8f4f3f00d2a3ea51bb20e98e4deb34e92e2
Yoren Chang <yorenchang@gmail.com>
2014-11-11 19:12:25 +0800

    Add filter toTrusted

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
-angular.module('app', ['ngRoute', 'ngSanitize'])
-.config(function($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
+var app = angular.module('app', ['ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
 
-	$routeProvider
-	.when('/', {
-		templateUrl: myLocalized.partials + 'main.html',
-		controller: 'Main'
-	})
-	.when('/demo', {
-		templateUrl: myLocalized.partials + 'demo.html',
-		controller: 'Main'
-	})
-	.when('/:ID', {
-		templateUrl: myLocalized.partials + 'content.html',
-		controller: 'Content'
-	});
-})
-.controller('Main', function($scope, $http, $routeParams) {
-	$http.get('wp-json/posts/').success(function(res){
-		$scope.posts = res;
-	});
-})
-.controller('Content', function($scope, $http, $routeParams) {
-	$http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
-		$scope.post = res;
-	});
-});
+        $routeProvider
+            .when('/', {
+                templateUrl: myLocalized.partials + 'main.html',
+                controller: 'Main'
+            })
+            .when('/demo', {
+                templateUrl: myLocalized.partials + 'demo.html',
+                controller: 'Main'
+            })
+            .when('/:ID', {
+                templateUrl: myLocalized.partials + 'content.html',
+                controller: 'Content'
+            });
+    })
+    .controller('Main', function($scope, $http, $routeParams) {
+        $http.get('wp-json/posts/').success(function(res) {
+            $scope.posts = res;
+        });
+    })
+    .controller('Content', function($scope, $http, $routeParams) {
+        $http.get('wp-json/posts/' + $routeParams.ID).success(function(res) {
+            $scope.post = res;
+        });
+    })
+    .filter('toTrusted', function($sce) {
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    });
