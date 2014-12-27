ef5e41448dc77081f9fac1250e9f080dea2a93be
Yoren Chang <yorenchang@gmail.com>
2014-12-27 15:43:37 +0800

    Add some simple code to demonstrate the <search-form> custom directive

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,28 @@ angular.module('app', ['ngRoute', 'ngSanitize'])
 	});
 })
 .controller('Main', function($scope, $http, $routeParams) {
+	$scope.filter = {
+		s: ''
+	};
+
 	$http.get('wp-json/posts/').success(function(res){
 		$scope.posts = res;
 	});
+
+	$scope.search = function() {
+		$http.get('wp-json/posts/?filter[s]=' + $scope.filter.s).success(function(res){
+			$scope.posts = res;
+		});
+	};
 })
 .controller('Content', function($scope, $http, $routeParams) {
 	$http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
 		$scope.post = res;
 	});
+})
+.directive('searchForm', function() {
+	return {
+		restrict: 'EA',
+		template: 'Search Keyword: <input type="text" name="s" ng-model="filter.s" ng-change="search()">',
+	};
 });
