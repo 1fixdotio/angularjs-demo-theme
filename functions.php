8e51cb6d5b1dd845f36cfe096afe8fc17dd541d3
Yoren Chang <yorenchang@gmail.com>
2015-02-06 17:34:44 +0800

    Make our theme work with admin bar

--- functions.php
+++ functions.php
@@ -34,4 +34,4 @@ function my_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
 
-add_filter('show_admin_bar', '__return_false');
+// add_filter('show_admin_bar', '__return_false');
--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
-<html ng-app="app">
+<html>
 <head>
 	<base href="/jsonapi/">
 	<title>AngularJS Demo Theme</title>
 	<?php wp_head(); ?>
 </head>
 <body>
+	<div id="page" ng-app="app">
+		<header>
+			<h1>
+				<a href="<?php echo site_url(); ?>">AngularJS Demo Theme</a>
+			</h1>
+		</header>
 
-	<header>
-		<h1>
-			<a href="<?php echo site_url(); ?>">AngularJS Demo Theme</a>
-		</h1>
-	</header>
+		<div ng-view></div>
 
-	<div ng-view></div>
-
-	<footer>
-		&copy; <?php echo date( 'Y' ); ?>
-	</footer>
-	<?php wp_footer(); ?>
+		<footer>
+			&copy; <?php echo date( 'Y' ); ?>
+		</footer>
+		<?php wp_footer(); ?>
+	</div>
 </body>
 </html>
